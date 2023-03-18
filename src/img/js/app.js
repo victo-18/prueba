a688023f32d96f05394a0fab33424a72fe353c7f
alomia18 <alomia18@gmail.com>
2023-03-18 05:10:23 -0500

    Revert "incorporando preloader"
    This reverts commit 92ced08a6f89ca870815bcd7b561563d8eda5eb8.

--- src/img/js/app.js
+++ src/img/js/app.js
@@ -2,7 +2,7 @@ window.onload=function(){
     //const carga = document.querySelector('#onload');
     //carga.fadeOut();
     // document.body.removeClass('heden');
-      alert("He cargado todo el documento....");
+
     $('#onload').fadeOut();
    
     $('body').removeClass('heden');
