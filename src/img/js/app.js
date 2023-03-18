01fc01836a273d42e5a3ec5b7019dd81b0a85b60
alomia18 <alomia18@gmail.com>
2023-03-17 23:12:45 -0500

    inicializando repo

---
+++ src/img/js/app.js
@@ -0,0 +1,9 @@
+// window.onload=function(){
+//     //const carga = document.querySelector('#onload');
+//     //carga.fadeOut();
+//     // document.body.removeClass('heden');
+
+//     $('#onload').fadeOut();
+   
+//     $('body').removeClass('heden');
+// }
